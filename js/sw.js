6ca380fb42a804594309c89e2a481664331c2fea
lqqppl <127760197@qq.com>
2017-12-22 15:59:16 +0800

    d

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,33 @@
-self.addEventListener('fetch', function(event) {
-  if (/\.jpg$/.test(event.request.url)) {
-    event.respondWith(fetch('../img/game.jpg'));
-  }
-});
+/*
+self: 表示 Service Worker 作用域, 也是全局变量
+caches: 表示缓存
+skipWaiting: 表示强制当前处在 waiting 状态的脚本进入 activate 状态
+clients: 表示 Service Worker 接管的页面
+ */
+
+ var cacheStorageKey = 'minimal-pwa-1'
+
+ var cacheList = [
+   '/',
+   "index.html",
+   "main.css",
+   "e.png"
+ ]
+ self.addEventListener('install', e => {
+   e.waitUntil(
+     caches.open(cacheStorageKey)
+     .then(cache => cache.addAll(cacheList))
+     .then(() => self.skipWaiting())
+   )
+ })
+
+ self.addEventListener('fetch', function(e) {
+   e.respondWith(
+     caches.match(e.request).then(function(response) {
+       if (response != null) {
+         return response
+       }
+       return fetch(e.request.url)
+     })
+   )
+ })
